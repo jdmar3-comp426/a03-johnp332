f891710ad864a3650126d52060c7a943d12f6cdb
johnp332 <johnp332@live.unc.edu>
2021-11-14 17:41:16 -0500

    finished mild part 1

--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -6,7 +6,10 @@
  * returns: { type: 'number', value: 4 }
  */
 export function identifyVariable(variable) {
-
+   return {
+      type: typeof variable,
+      value: variable
+   } 
 }
 
 
@@ -24,7 +27,14 @@ export function identifyVariable(variable) {
 
  */
 export function identifyArray(array) {
-
+   let type_arr = [];
+   array.forEach(element => {
+      let type = {};
+      type.type = typeof element;
+      type.value = element;
+      type_arr.push(type);
+   });
+   return type_arr;
 }
 
 /**
@@ -44,7 +54,7 @@ export function identifyArray(array) {
  obj now does not contain the `password` field
  */
 export function removeKey(object, key) {
-
+   delete object[key];
 }
 
 /**
@@ -64,7 +74,9 @@ export function removeKey(object, key) {
  If only `removeKeyNonDestructive` was called, nothing would have changed.
  */
 export function removeKeyNonDestructive(object, key) {
-
+   const cloneObj = Object.assign({}, object);
+   delete cloneObj[key];
+   return cloneObj;
 }
 
 /**
@@ -89,5 +101,9 @@ export function removeKeyNonDestructive(object, key) {
  * @return {*} The object with its keys removed.
  */
 export function removeKeys(object, keyList) {
-
+   const cloneObj = Object.assign({}, object);
+   for(let i = 0; i < keyList.length; i++) {
+      delete cloneObj[keyList[i]];
+   }
+   return cloneObj
 }
